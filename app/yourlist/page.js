b4967d786b5002440bbb1bf2262e85680b7dd9d4
minusone <sgone2842@gmail.com>
2025-09-06 11:20:40 +0530

    redesigning dashboard

--- app/yourlist/page.js
+++ app/yourlist/page.js
@@ -50,21 +50,33 @@ const page = () => {
 
   return (
     <>
-    <div>
-        <h1>Enter your channel links here</h1>
+    <div className='min-h-screen bg-gradient-to-r from-[#FF8235] to-[#30E8BF] p-4'>
+        <div className='flex flex-col items-center justify-center min-h-screen'>
+        <h1 className='text-2xl md:text-5xl font-bold
+         text-white text-center drop-shadow-lg py-5'>
+        Enter your channel links here</h1>
 
-        <textarea
-        className='w-full h-40 p-2 border border-gray-300 rounded-lg'
-        placeholder='Enter your channel links here'
-        rows={4}
-        value={text}
-        onChange={(e) => setText(e.target.value)}
+        <textarea className="w-full md:w-1/2 lg:w-1/3 h-40 p-4 rounded-2xl shadow-md 
+                 bg-gradient-to-r from-gray-800 to-gray-700  text-white placeholder-gray-400 
+                 focus:outline-none focus:ring-2 focus:ring-teal-400 focus:ring-offset-2 focus:ring-offset-gray-900
+                 transition-all duration-300 ease-in-out resize-none"
+            placeholder="Enter your channel links here..."
+            rows={4}
+            value={text}
+            onChange={(e) => setText(e.target.value)}
         ></textarea>
 
+
         <button
-        className='bg-blue-500 text-white p-2 rounded-lg ml-4'
+        className='px-8 py-3 rounded-xl font-semibold text-white 
+                   bg-gradient-to-r from-red-400 to-pink-500 
+                   shadow-md hover:shadow-lg 
+                   transition-all duration-300 
+                   hover:scale-105 active:scale-95
+                   focus:outline-none focus:ring-2 focus:ring-pink-400'
         onClick={() => handleSubmit()}
         >Submit</button>
+        </div>  
     </div>
     </>
   )
